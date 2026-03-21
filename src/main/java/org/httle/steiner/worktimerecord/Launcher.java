46ff20518a70f4b8256d9d6f4ec18ea675c78ec2
dani.exe <daniiexe@outlook.com>
2026-03-21 10:27:33 +0100

    Styling changed

---
+++ src/main/java/org/httle/steiner/worktimerecord/Launcher.java
@@ -0,0 +1,7 @@
+package org.httle.steiner.worktimerecord;
+
+public class Launcher {
+    public static void main(String[] args) {
+        WorkTimeRecord.launch(WorkTimeRecord.class, args);
+    }
+}
